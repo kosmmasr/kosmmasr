b634c083bf6945d5cb452fc6f0b523c12bddc063
kosmmasr <44833001+kosmmasr@users.noreply.github.com>
2018-12-13 12:23:48 +0200

    Update Dream.js

--- Dream.js
+++ Dream.js
@@ -1,14 +1,1 @@
-﻿const Discord = require('discord.js');
-const client = new Discord.Client();
-console.log("Scrpit By Dream");
 
-
-client.on("ready", () => {
-let channel =     client.channels.get("509546274887041025")
-setInterval(function() {
-channel.send(`ube youtube `);
-}, 25)
-})
- 
- 
-client.login('NDUxNDYzODExODcwNzUyNzc4.Dr1DcQ.2z9uFamw5vRAqwe-l39e2OlbAo8');
